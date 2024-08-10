b351b31632e82eb9a85b181b73bf598c645c6457
Ravid Levy <61316827+Ravid-Levy@users.noreply.github.com>
2024-08-10 18:15:50 +0300

    fix

--- sb/test-app.py
+++ sb/test-app.py
@@ -2,18 +2,22 @@ from gui import *
 from kivy.app import App
 from widget_screen import WidgetScreen
 
+
 class TestScreen(WidgetScreen):
 
     def __init__(self, **kwargs):
         super(TestScreen, self).__init__(**kwargs)
         self.load_screen()
 
+
 class TestApp(App):
 
     def build(self):
         Window.clearcolor = 'steelblue'
-        self.test_screen = TestScreen(file_name = 'test.json')
+        self.test_screen = TestScreen(file_name='test.json')
         return self.test_screen
+
 
 if __name__ == '__main__':
     TestApp().run()
+ 
